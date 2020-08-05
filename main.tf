aeea65cebbfdfdb7a3c5bb43f309adb29782dff1
lakshmi.atluri <atluri.lakshmi1993@gmail.com>
2020-08-05 09:03:38 -0500

    terraform script to create s3 bucket

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+    region = "us-east-1"
+    access_key = "AKIAQIPEREB6IW2P3FOY"
+    secret_key = "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"
+
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket89900"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+  }
+}
